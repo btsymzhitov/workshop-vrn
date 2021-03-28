f568367f9e4b7163e09b1d20067649b0643dee29
Bulat Tsymzhiov <b.tsymzhitov@anchorfree.com>
2021-03-28 07:14:40 -0700

    new project for study

---
+++ internal/handler/handler.go
@@ -0,0 +1,15 @@
+package handler
+
+type Handler struct {
+	
+}
+
+func NewHandler() *Handler{
+	return &Handler{}
+}
+
+func(h *Handler) Hello (w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "hello golang Voronezh")
+
+}
+
